59048b970dd8b570829560044af2e841845700d3
thaise <thaisefloripa@gmail.com>
2011-11-20 17:04:28 -0200

    Testes - orientação a objetos - carro

---
+++ Motor.java
@@ -0,0 +1,12 @@
+package br.com.xti.poo;
+
+public class Motor {
+
+	String tipo;
+	int potencia;
+	
+	public Motor(String tipo, int potencia){//construtor
+		this.tipo = tipo;
+		this.potencia = potencia;
+	}
+}
