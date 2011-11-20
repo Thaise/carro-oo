a7d5f7a780ca98cba3d36d00503cac298cc06d2a
Thaise <thaisefloripa@gmail.com>
2011-11-20 17:12:59 -0200

    Testes - orientação a objetos - carro

--- CarroTeste.java
+++ CarroTeste.java
@@ -6,9 +6,6 @@ public class CarroTeste {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/*esse é o modo de inicializar as
-		 *cariáveis em um construtor sem parâmetros
-		 */
 		Carro ferrari = new Carro();
 		ferrari.modelo = "Ferrari Enzo";
 		ferrari.velocidadeMaxima = 349;
@@ -17,12 +14,6 @@ public class CarroTeste {
 		Motor v12 = new Motor("v12", 660);
 		ferrari.motor = v12;
 	
-		/*
-		 *num construtor com parâmetros
-		 *podemos inicializar as variáveis
-		 *dentro no espaço diretamente nos 
-		 *parâmetros, atribuindo valor à eles
-		 */
 		Carro koenigsegg = new Carro("koenigsegg CCXR", 430, 3.1);
 		
 		Motor v8 = new Motor("v8", 1018);
